d1c83540b2cd2c9c22379edefffa7f32712234e5
Kijin Nam <knam@water.ca.gov>
2024-09-09 16:57:54 -0700

    Switch to a better test data
    The previous data set from HelloSCHISM uses non-LSC2 triangle-only grid.
    It does not cover good cases, so the tests are updated with a new data
    set that is cropped from a Bay-Delta SCHISM.

--- tests/test_suxarray.py
+++ tests/test_suxarray.py
@@ -13,8 +13,8 @@ def test_read_grid():
     ds_out2d = xr.open_dataset(str(p_cur / "testdata/out2d_1.nc"))
     ds_zcoords = xr.open_dataset(str(p_cur / "testdata/zCoordinates_1.nc"))
     grid = sx.read_grid(ds_out2d, ds_zcoords)
-    assert grid.n_node == 2639
-    assert grid.n_face == 4636
+    assert grid.n_node == 823
+    assert grid.n_face == 791
 
 
 def test_read_schism_nc():
@@ -25,9 +25,9 @@ def test_read_schism_nc():
     grid = sx.read_grid(ds_out2d, ds_zcoords)
     ds_data = xr.open_dataset(str(p_cur / "testdata/salinity_1.nc"))
     sxds = sx.read_schism_nc(grid, ds_data)
-    assert sxds.sxgrid.n_node == 2639
-    assert sxds.sxgrid.n_face == 4636
-    assert sxds.time.size == 72
+    assert sxds.sxgrid.n_node == 823
+    assert sxds.sxgrid.n_face == 791
+    assert sxds.time.size == 48
 
 
 def test_read_hgrid_gr3():
@@ -67,22 +67,32 @@ def test_find_nearest_node(grid_test):
 
 def test_subset_bounding_polygon(sxds_test_dask):
     """Test subsetting dataarray with a polygon"""
-    polygon = Polygon([(0.0, 0.0), (1000.0, 0.0), (1000.0, 10400.0), (0.0, 10400.0)])
+    # polygon = Polygon([(0.0, 0.0), (1000.0, 0.0), (1000.0, 10400.0), (0.0, 10400.0)])
+    polygon = Polygon(
+        [
+            (569866.8953279585, 4213065.012236144),
+            (570131.3752643355, 4213065.012236144),
+            (570131.3752643355, 4213262.690668024),
+            (569866.8953279585, 4213262.690668024),
+        ]
+    )
     da_subset = sxds_test_dask["salinity"].subset.bounding_polygon(polygon)
-    assert da_subset.uxgrid.n_node == 21
+    assert da_subset.uxgrid.n_node == 15
+    assert da_subset.uxgrid.n_face == 10
 
 
 def test_subset_bounding_box(sxds_test_dask):
     """Test find_element_at"""
     da_subset = sxds_test_dask["salinity"].subset.bounding_box_xy(
-        [0.0, 1000.0], [0.0, 10400.0]
+        [569866.8953279585, 570131.3752643355], [4213065.012236144, 4213262.690668024]
     )
-    assert da_subset.uxgrid.n_node == 21
+    assert da_subset.uxgrid.n_node == 15
+    assert da_subset.uxgrid.n_face == 10
 
 
 def test_isel(sxds_test_dask):
     da = sxds_test_dask["salinity"].isel(time=range(2))
-    assert da.uxgrid.n_node == 2639
+    assert da.uxgrid.n_node == 823
     assert da.uxgrid.sgrid_info.time.size == 2
     # Slice again
     da = sxds_test_dask["salinity"].isel(time=range(4))
@@ -91,15 +101,23 @@ def test_isel(sxds_test_dask):
 
 def test_depth_average(sxds_test_dask):
     da = sxds_test_dask["salinity"].depth_average()
-    assert da.sel(n_node=492).values[0] == pytest.approx(0.145977, abs=1e-6)
+    # The value is calculated from VisIt
+    assert da.sel(n_node=439).values[0] == pytest.approx(11.961535, abs=1e-6)
 
 
 def test_slice_and_depth_average(sxds_test_dask):
-    polygon = Polygon([(0.0, 0.0), (1000.0, 0.0), (1000.0, 10400.0), (0.0, 10400.0)])
+    polygon = Polygon(
+        [
+            (569866.8953279585, 4213065.012236144),
+            (570131.3752643355, 4213065.012236144),
+            (570131.3752643355, 4213262.690668024),
+            (569866.8953279585, 4213262.690668024),
+        ]
+    )
     da_subset = sxds_test_dask["salinity"].subset.bounding_polygon(polygon)
     da_da = da_subset.depth_average()
-    # Node 492 is now 1
-    assert da_da.sel(n_node=1).values[0] == pytest.approx(0.145977, abs=1e-6)
+    # Node 186 is now 7
+    assert da_da.sel(n_node=7).values[0] == pytest.approx(10.334445, abs=1e-6)
 
 
 # def test_interpolate_xy(sxds_test_dask):
